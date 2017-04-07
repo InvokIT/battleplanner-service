616f972828a962aaf91a31b89ee6b7c9c776fe90
Sebastian Jørgensen <sj@invokit.dk>
2017-04-07 20:42:18 +0100

    Added continue change to choose-initiator state

--- src/gcs/states/choose-initiator.js
+++ src/gcs/states/choose-initiator.js
@@ -1,6 +1,7 @@
 const cloneDeep = require("lodash/fp/cloneDeep");
 const isNumber = require("lodash/fp/isNumber");
 const flow = require("lodash/fp/flow");
+const isNil = require("lodash/fp/isNil");
 const log = require("../../log")(__filename);
 const {setInitiator, setCurrentTeam} = require("./state-util");
 
@@ -18,15 +19,39 @@ class ChooseInitiator {
         }
 
         const nextState = {
-            name: "select-map-or-faction",
+            name: "choose-initiator",
             data: flow(
                 cloneDeep,
-                setInitiator(team),
-                setCurrentTeam(team)
+                setInitiator(team)
             )(this.data)
         };
 
         log.info({team, user, nextState}, "User chose initiating team.");
+
+        return nextState;
+    }
+
+    flipCoin({user}) {
+        log.trace({this: this, user}, "flipCoin");
+
+        const randomTeam = Math.floor(Math.random() * this.data.teams.length);
+        return this.chooseInitiator({team: randomTeam, user: user});
+    }
+
+    continue({user}) {
+        log.trace({user}, "continue");
+
+        if (isNil(this.data.initiator)) {
+            throw new Error("Cannot continue without an initator set.");
+        }
+
+        const nextState =  {
+            name: "select-map-or-faction",
+            data: flow(
+                cloneDeep,
+                setCurrentTeam(this.data.initiator)
+            )(this.data)
+        };
 
         return nextState;
     }
